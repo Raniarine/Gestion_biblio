67a30bad8d9ac8a6abd95c14983872976cb1d9f7
Raniarine <125674022+Raniarine@users.noreply.github.com>
2025-05-28 15:34:05 +0100

    Update bibliotheque.py

--- bibliotheque.py
+++ bibliotheque.py
@@ -1,5 +1,5 @@
-from Livre import Livre
-from Utilisateur import Utilisateur
+from livre import Livre
+from utilisateur import Utilisateur
 
 class Bibliotheque:
     def __init__(self):
@@ -10,42 +10,14 @@ class Bibliotheque:
         self.livres.append(livre)
         print("Livre ajouté avec succès.")
 
-    def enregistrer_utilisateur(self, nom, identifiant):
-        utilisateur = Utilisateur(nom, identifiant)
+    def ajouter_utilisateur(self, utilisateur):
         self.utilisateurs.append(utilisateur)
-        print("Utilisateur enregistré.")
+        print("Utilisateur ajouté avec succès.")
 
     def lister_livres(self, uniquement_disponibles=False):
         for livre in self.livres:
             if not uniquement_disponibles or livre.est_disponible():
                 livre.afficher_info()
-
-    def trouver_livre(self, isbn):
-        for livre in self.livres:
-            if livre.get_isbn() == isbn:
-                return livre
-        return None
-
-    def trouver_utilisateur(self, identifiant):
-        for utilisateur in self.utilisateurs:
-            if utilisateur.identifiant == identifiant:
-                return utilisateur
-        return None
-
-    def emprunter_livre(self, identifiant, isbn):
-        utilisateur = self.trouver_utilisateur(identifiant)
-        livre = self.trouver_livre(isbn)
-        if utilisateur and livre:
-            utilisateur.emprunter_livre(livre)
-        else:
-            print("Utilisateur ou livre non trouvé.")
-
-    def rendre_livre(self, identifiant, isbn):
-        utilisateur = self.trouver_utilisateur(identifiant)
-        if utilisateur:
-            utilisateur.rendre_livre(isbn)
-        else:
-            print("Utilisateur non trouvé.")
 
     def afficher_utilisateurs(self):
         for utilisateur in self.utilisateurs:
